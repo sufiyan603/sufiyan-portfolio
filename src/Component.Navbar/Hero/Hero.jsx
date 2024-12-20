cb514de3ff8dee160eb359e28cb2b448f6c12b97
sufiyan603 <sufiaynrazapathan51800@gmail.com>
2024-12-20 17:42:05 +0530

    p tag remove form hero.jsx

--- src/Component.Navbar/Hero/Hero.jsx
+++ src/Component.Navbar/Hero/Hero.jsx
@@ -7,7 +7,6 @@ const Hero = () => {
     <div id='home' className='hero'>
       <div className='img1'><img src={myImage}></img><br /></div>
       <h1><span>I'm Sufiyan Raza Khan</span>, frontend developer based in Ind. </h1><br />
-      <p>I am frontend developer from maharashtra, india fresher & looking for opportunity</p><br />
       <div className='hero-action'>
         <AnchorLink className='anchor-link' href='#contact'><div className='hero-connect'>Contact Me</div></AnchorLink>
         <a href="resume.pdf.pdf" target="_blank" className='hero-resume' >
